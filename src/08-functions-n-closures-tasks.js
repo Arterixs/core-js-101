8fac2c55f25c23940c1f88c1bbc7a888e16a69f6
Arterixs <artemontugg@gmail.com>
2023-01-02 02:54:05 +0300

    feat: add one task solutions 08

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -44,8 +44,11 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const count = exponent;
+  return function r(num) {
+    return num ** count;
+  };
 }
 
 
